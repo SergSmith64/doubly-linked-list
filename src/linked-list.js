9ed58db9a2c0d521d26d79b6220811eaed94044c
SergSmith64 <sergsmith2008@yandex.ru>
2019-10-12 19:53:06 +0400

    fix 6 from 15

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,58 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.lenght = 0;
+        this._head = 0;
+        this._tail = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        const node = new Node(data, this._tail);
+        if (this._tail) this._tail.next = node;
+        this._tail = node;
+        if (!this._head) this._head = node;
+        this.length++;
+        return this;
+    }
 
-    head() {}
+    head() {
+        return this._head ? this._head.data : null;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail ? this._tail.data : null;
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        return !this._head;
+    }
 
-    clear() {}
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this.lenght = 0;
+        return this;
+    }
 
     deleteAt(index) {}
 
     reverse() {}
 
-    indexOf(data) {}
-}
+    indexOf(data) {
+        let curr = this._head;
+        for (let i = 0; curr !== null; i++) {
+          if (curr.data === data) {
+            return i;
+          }
+          curr = curr.next;
+        }
+        return -1;
+      }
+    }
 
 module.exports = LinkedList;
